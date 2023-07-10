f846b7abd47f7951e23473890b81dff2e1038261
laurakeeley <l.keeley000@gmail.com>
2023-07-10 15:53:07 -0500

    changed to deprecated CanActivate

--- src/app/service/route-guard.service.ts
+++ src/app/service/route-guard.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
-import { RouterStateSnapshot } from '@angular/router';
-import { ActivatedRouteSnapshot } from '@angular/router';
-import { CanActivateFn } from '@angular/router';
+// import { RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+// import { CanActivateFn } from '@angular/router';
 import { HardcodedAuthenticationService } from './hardcoded-authentication.service';
+// import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouteGuardService implements CanActivateFn {
+export class RouteGuardService implements CanActivate {
 
   constructor(public hardcodedAuthenticationService: HardcodedAuthenticationService) { }
 
-  canActivateFn(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (this.hardcodedAuthenticationService.isUserLoggedIn()) {
       return true;
     }
